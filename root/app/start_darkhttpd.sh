a9d7b8d358a63465d4ce4d46ca2763432f365108
Thomas Fanninger <thomas@fanninger.at>
2018-02-18 12:28:32 +0100

    Initial changes

--- root/app/start_darkhttpd.sh
+++ root/app/start_darkhttpd.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-/usr/bin/darkhttpd /aria2-webui --port 80 &&
-/usr/bin/aria2c --conf-path=/config/aria2.conf
+/usr/bin/darkhttpd /aria2-webui --port 80
--- root/etc/cont-init.d/91_configure_darkhttpd.sh
+++ root/etc/cont-init.d/91_configure_darkhttpd.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/with-contenv bash
 
-dockerize -template /app/start_darkhttpd.sh:/config/start.sh
-
-chown -R app:users /aria2-webui
-chown -R app:users /config/start.sh
+dockerize -template /app/start_darkhttpd.sh:/config/start_darkhttpd.sh
 
